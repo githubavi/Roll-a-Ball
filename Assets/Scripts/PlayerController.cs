9f156d8a6446e3e05b78e99a73a008128b423c14
githubavi <asama1@sapient.com>
2016-10-20 20:17:06 +0530

    roll a ball hololens unity game
    roll a ball hololens unity game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+    int count = 0;
+    public Text countText;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame, beofre rendering each frame
+	void Update () {
+	
+	}
+
+    //called just, beofore performing any  physics calculation
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            countText.text = count.ToString();
+        }
+        //Destroy(other.gameObject);
+    }
+
+}
